61d22d9eecd3873cdb6f1cb4d184b1cdde1ab693
roronan13 <ronan.pothier@gmail.com>
2024-03-08 15:17:19 +0100

    8 mar

--- push_swap_errors.c
+++ push_swap_errors.c
@@ -6,7 +6,7 @@
 /*   By: rpothier <rpothier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/29 17:11:22 by rpothier          #+#    #+#             */
-/*   Updated: 2024/03/07 22:11:13 by rpothier         ###   ########.fr       */
+/*   Updated: 2024/03/08 15:17:13 by rpothier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 void	check_errors(int argc, char **argv)
 {
 	check_params(argc, argv);
-	check_number(argc, argv);
+	if (!check_number(argc, argv));
 }
 
 void	check_params(int argc, char **argv)
@@ -24,7 +24,26 @@ void	check_params(int argc, char **argv)
 		exit(EXIT_SUCCESS);
 }
 
-void	check_number(int argc, char **argv)
+int	check_number(int argc, char **argv)
 {
-	while ()
+	int	i;
+	int	j;
+
+	i = 1;
+	while (i < argc)
+	{
+		j = 0;
+		while (argv[i][j])
+		{
+			if (j >= 0 && j <= 9)
+				j++;
+			else
+			{
+				write(1, "Error\n", 6);
+				return (0);
+			}
+		}
+		i++;
+	}
+	return (1);
 }
