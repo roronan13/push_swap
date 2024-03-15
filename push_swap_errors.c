cd447b1503c8007c10ac9521b0feec34da1c93ff
roronan13 <ronan.pothier@gmail.com>
2024-03-15 16:02:23 +0100

    15 mar

--- push_swap_errors.c
+++ push_swap_errors.c
@@ -6,7 +6,7 @@
 /*   By: rpothier <rpothier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/29 17:11:22 by rpothier          #+#    #+#             */
-/*   Updated: 2024/03/15 02:16:50 by rpothier         ###   ########.fr       */
+/*   Updated: 2024/03/15 16:01:19 by rpothier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -80,9 +80,8 @@ void	check_twice(int argc, char **argv)
 
 	i = 0;
 	nbr_table = fill_int_table(argc, argv, nbr_table);
-	while (nbr_table[i])
+	while (nbr_table[i] > INT_MIN && i <= (argc - 3))
 	{
-		//printf("%d\n", INT_MAX);
 		printf("%d\n", nbr_table[i]);
 		j = i + 1;
 		while (nbr_table[j])
--- push_swap_utils.c
+++ push_swap_utils.c
@@ -6,7 +6,7 @@
 /*   By: rpothier <rpothier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/08 17:03:15 by rpothier          #+#    #+#             */
-/*   Updated: 2024/03/15 02:14:11 by rpothier         ###   ########.fr       */
+/*   Updated: 2024/03/15 15:55:34 by rpothier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,11 +65,9 @@ int	*fill_int_table(int argc, char **argv, int *nbr_table)
 {
 	int	i;
 	int	j;
-	int	tableau[4];
 
 	i = 0;
 	j = 1;
-	//printf("%d\n", argc);
 	nbr_table = malloc(sizeof(int) * (argc - 1));
 	if (!nbr_table)
 	{
@@ -78,52 +76,11 @@ int	*fill_int_table(int argc, char **argv, int *nbr_table)
 		write(2, "Error 3\n", 8);
 		exit(EXIT_FAILURE);
 	}
-	size_t taille;
-	taille = sizeof(int) * (argc - 1);
-	//printf("taille tableau 2 : %ld\n", taille);
-	//*argv++;
-	//while (*argv)
 	while (argc - j)
 	{
-		/* printf("argv : %s\n", argv[j]);
-		printf("nbr_table : %d\n", nbr_table[i]); */
 		nbr_table[i] = ft_atoi(argv[j]);
-		//printf("%s\n", *argv);
-		/* printf("argv : %s\n", argv[j]);
-		printf("nbr_table : %d\n", nbr_table[i]); */
 		i++;
-		//*argv++;
 		j++;
 	}
-	/* i = 0;
-	while (nbr_table[i])
-	{
-		printf("%d\n", nbr_table[i]);
-		i++;
-	} */
-	/* i = 0;
-	while (nbr_table[i])
-	{
-		printf("%d\n", nbr_table[i]);
-		i++;
-	} */
-	/* i = 0;
-	printf("%d\n", nbr_table[i]);
-	printf("%d\n", nbr_table[i + 1]);
-	printf("%d\n", nbr_table[i + 2]);
-	printf("%d\n", nbr_table[i + 3]);
-	printf("%d\n", nbr_table[i + 4]);
-	printf("%d\n", nbr_table[i + 5]);
-	printf("%d\n", nbr_table[i + 6]);
-	printf("%d\n", nbr_table[i + 7]);
-	printf("%d\n", nbr_table[i + 8]); */
-	/* printf("argv : %s\n", argv[j+ 1]);
-	printf("nbr_table : %d\n", nbr_table[i + 1]);
-	printf("argv : %s\n", argv[j+ 2]);
-	printf("nbr_table : %d\n", nbr_table[i + 2]);
-	printf("argv : %s\n", argv[j+ 3]);
-	printf("nbr_table : %d\n", nbr_table[i + 3]);
-	printf("argv : %s\n", argv[j+ 4]);
-	printf("nbr_table : %d\n", nbr_table[i + 4]); */
 	return (nbr_table);
 }
