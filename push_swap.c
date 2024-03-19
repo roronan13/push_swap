26383adcb7b68e8ed378a3fd8aa33d7ca63908c6
roronan13 <ronan.pothier@gmail.com>
2024-03-19 21:17:33 +0100

    19 mar

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: rpothier <rpothier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/29 13:35:38 by rpothier          #+#    #+#             */
-/*   Updated: 2024/03/19 20:01:00 by rpothier         ###   ########.fr       */
+/*   Updated: 2024/03/19 20:32:40 by rpothier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,11 @@
 int	main(int argc, char **argv)
 {
 	check_errors(argc, argv);
-	
+	create_list(argc, argv);
 	return (0);
+}
+
+list_element	create_list(int argc, char **argv)
+{
+	
 }
