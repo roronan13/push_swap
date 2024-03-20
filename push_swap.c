4a8ead54e191f76ece30bf474a1832b86915324f
roronan13 <ronan.pothier@gmail.com>
2024-03-21 00:29:59 +0100

    20 mar

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: rpothier <rpothier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/29 13:35:38 by rpothier          #+#    #+#             */
-/*   Updated: 2024/03/20 21:36:18 by rpothier         ###   ########.fr       */
+/*   Updated: 2024/03/21 00:29:25 by rpothier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@ int	main(int argc, char **argv)
 
 	check_errors(argc, argv);
 	nbr_list = create_list(argc, argv);
+	allo(nbr_list);
 	return (0);
 }
 
@@ -26,39 +27,48 @@ t_list_element	*create_list(int argc, char **argv)
 {
 	t_list_element	*head;
 	t_list_element	*new_element;
+	t_list_element	*previous_element;
 	
 	head = malloc(sizeof(t_list_element));
 	if (!head)
 		return (NULL);
 	*argv++;
+	//printf("%s\n", *argv);
 	head->content = ft_atoi(*argv);
 	head->next = NULL;
 	head->previous = NULL;
 	new_element = head;
+	*argv++;
 	while (*argv && argc)
 	{
 		new_element->next = malloc(sizeof(t_list_element));
 		if (!new_element->next)
 			return (NULL);
+		previous_element = new_element;
 		new_element = new_element->next;
 		new_element->content = ft_atoi(*argv);
 		new_element->next = NULL;
+		new_element->previous = previous_element;
 		*argv++;
 	}
-	//new_element->next = head;
+	new_element->next = head;
+	head->previous = new_element;
 	return (head);
 }
 
 void	allo(t_list_element *head)
 {
 	t_list_element *temp;
-	int	i = 0;
+	int	i;
+	int	j;
 
 	temp = head;
-	while (temp)
+	i = temp->content;
+	j = i + 1;
+	while (j != i)
 	{
 		printf("%d\n", temp->content);
 		temp = temp->next;
-		i++;
+		j = temp->content;
 	}
 }
