7c98935b53874e4a41354544eb88479cf4435026
roronan13 <ronan.pothier@gmail.com>
2024-03-20 18:32:04 +0100

    20 mar

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: rpothier <rpothier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/29 13:35:38 by rpothier          #+#    #+#             */
-/*   Updated: 2024/03/20 18:29:50 by rpothier         ###   ########.fr       */
+/*   Updated: 2024/03/20 18:31:56 by rpothier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 int	main(int argc, char **argv)
 {
 	t_list_element	**nbr_list;
-	
+
 	*nbr_list = NULL;
 	check_errors(argc, argv);
 	nbr_list = malloc(sizeof(t_list_element));
