51521a2927205f7ad764559548ef17de6a953093
roronan13 <ronan.pothier@gmail.com>
2024-03-20 18:30:54 +0100

    20 mar

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: rpothier <rpothier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/29 13:35:38 by rpothier          #+#    #+#             */
-/*   Updated: 2024/03/20 18:02:09 by rpothier         ###   ########.fr       */
+/*   Updated: 2024/03/20 18:29:50 by rpothier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,18 @@
 
 int	main(int argc, char **argv)
 {
-	t_list_element	*nbr_list;
+	t_list_element	**nbr_list;
 	
+	*nbr_list = NULL;
 	check_errors(argc, argv);
 	nbr_list = malloc(sizeof(t_list_element));
-	nbr_list = create_list(argc, argv, &nbr_list);
+	nbr_list = create_list(argc, argv, nbr_list);
 	return (0);
 }
 
-t_list_element	*create_list(int argc, char **argv, t_list_element *nbr_list)
+t_list_element	**create_list(int argc, char **argv, t_list_element **nbr_list)
 {
-	
+	t_list_element	*new_element;
+
+	new_element = *nbr_list;
 }
--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: rpothier <rpothier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/29 14:24:29 by rpothier          #+#    #+#             */
-/*   Updated: 2024/03/20 16:11:18 by rpothier         ###   ########.fr       */
+/*   Updated: 2024/03/20 18:21:42 by rpothier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,14 +24,14 @@ typedef struct list
 	struct list	*previous;
 }	t_list_element;
 
-void	check_errors(int argc, char **argv);
-void	check_params(int argc, char **argv);
-void	check_number(int argc, char **argv);
-void	check_long(int argc, char **argv);
-void	check_twice(int argc, char **argv);
-int		ft_atoi(char *argv);
-void	check_overflow(char *argv, int i, int resu);
-int		*fill_int_table(int argc, char **argv);
-t_list_element	*create_list(int argc, char **argv, t_list_element *nbr_list);
+void			check_errors(int argc, char **argv);
+void			check_params(int argc, char **argv);
+void			check_number(int argc, char **argv);
+void			check_long(int argc, char **argv);
+void			check_twice(int argc, char **argv);
+int				ft_atoi(char *argv);
+void			check_overflow(char *argv, int i, int resu);
+int				*fill_int_table(int argc, char **argv);
+t_list_element	**create_list(int argc, char **argv, t_list_element **nbr_list);
 
 #endif
