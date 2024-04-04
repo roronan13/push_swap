1ce933585ec4902083c3f8f86b9840c34b76a7cf
roronan13 <ronan.pothier@gmail.com>
2024-04-04 22:05:21 +0200

    4 avr

--- instructions/rotate_a.c
+++ instructions/rotate_a.c
@@ -6,7 +6,7 @@
 /*   By: rpothier <rpothier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/28 22:52:42 by rpothier          #+#    #+#             */
-/*   Updated: 2024/03/28 22:53:19 by rpothier         ###   ########.fr       */
+/*   Updated: 2024/04/04 22:05:11 by rpothier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,14 @@
 
 void	rotate_a(t_list_element *head)
 {
-	
+	t_list_element	*temp_ptr;
+	int				temp_int;
+
+	temp_ptr = head;
+	temp_int = temp_ptr->previous->content;
+	while (temp_ptr->previous != head)
+	{
+		temp_ptr->previous->content = temp_int;
+		temp_ptr = temp_ptr->previous;
+	}
 }
