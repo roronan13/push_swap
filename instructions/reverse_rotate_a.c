dd858f09b422e263b2b86ee142f48ed51ad7b656
roronan13 <ronan.pothier@gmail.com>
2024-04-05 00:36:17 +0200

    4 avr

--- instructions/reverse_rotate_a.c
+++ instructions/reverse_rotate_a.c
@@ -6,7 +6,7 @@
 /*   By: rpothier <rpothier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/05 00:19:12 by rpothier          #+#    #+#             */
-/*   Updated: 2024/04/05 00:20:18 by rpothier         ###   ########.fr       */
+/*   Updated: 2024/04/05 00:35:59 by rpothier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,20 @@
 
 void	reverse_rotate_a(t_list_element *head)
 {
-	
+	t_list_element	*temp_ptr;
+	int				int_1;
+	int				int_2;
+
+	temp_ptr = head->next;
+	int_1 = head->content;
+	while (temp_ptr != head)
+	{
+		int_2 = temp_ptr->content;
+		temp_ptr->content = int_1;
+		int_1 = int_2;
+		temp_ptr = temp_ptr->next;
+	}
+	temp_ptr->content = int_1;
+	temp_ptr = NULL;
+	write(1, "rra\n", 4);
 }
