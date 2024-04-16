b29bd3c3b7c69c02c5a5b2c103d8877a3199759e
roronan13 <ronan.pothier@gmail.com>
2024-04-16 16:19:56 +0200

    16 avr

--- instructions/swap_a_swap_b.c
+++ instructions/swap_a_swap_b.c
@@ -6,7 +6,7 @@
 /*   By: rpothier <rpothier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/12 16:32:12 by rpothier          #+#    #+#             */
-/*   Updated: 2024/04/12 18:39:17 by rpothier         ###   ########.fr       */
+/*   Updated: 2024/04/16 16:19:28 by rpothier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,7 @@
 
 void	swap_a_swap_b(t_list_element *a_head, t_list_element *b_head)
 {
-	
+	swap_a(a_head);
+	swap_b(b_head);
+	write(1, "ss\n", 3);
 }
