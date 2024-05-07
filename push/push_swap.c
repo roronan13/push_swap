3cd822d41ea87b3fd79794e5511bf7348e55c1e3
roronan13 <ronan.pothier@gmail.com>
2024-05-07 19:46:28 +0200

    7 mai

--- push/push_swap.c
+++ push/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: rpothier <rpothier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/29 13:35:38 by rpothier          #+#    #+#             */
-/*   Updated: 2024/05/07 19:15:53 by rpothier         ###   ########.fr       */
+/*   Updated: 2024/05/07 19:46:25 by rpothier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,14 +22,16 @@ int	main(int argc, char **argv)
 	b_head = NULL;
 	set_final(a_head);
 	set_group(a_head);
-	
-	if (argc == 4)
-		sort_three(a_head);
-	else
+	if (!is_sorted(a_head))
 	{
-		sort(&a_head, &b_head);
-		sort_three(a_head);
-		sort_back(&a_head, &b_head);
+		if (argc == 4)
+			sort_three(a_head);
+		else
+		{
+			sort(&a_head, &b_head);
+			sort_three(a_head);
+			sort_back(&a_head, &b_head);
+		}
 	}
 
 	allo_a(a_head);
